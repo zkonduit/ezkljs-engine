ca7678ce98d4fff18d1a69f102a867f7f262997c
Ethan Cemer <tylercemer@gmail.com>
2023-08-17 22:03:25 -0500

    *updated readme

--- app/components/ElgamalRandomVar.tsx
+++ app/components/ElgamalRandomVar.tsx
@@ -8,7 +8,7 @@ export default function ElgamalRandomVar() {
 
   return (
     <div>
-      <h1>Generate Elgamal Variable</h1>
+      <h1>Generate Elgamal Variables</h1>
       <button
         id='genREVButton'
         onClick={() => {

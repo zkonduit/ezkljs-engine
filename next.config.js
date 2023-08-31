5f0263dec020b36700b1fa341813a82cbecad419
Ethan Cemer <tylercemer@gmail.com>
2023-08-31 18:53:24 -0500

    *add CORP header.

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
                         key: 'Cross-Origin-Opener-Policy',
                         value: 'same-origin',
                     },
+                    {
+                        key: 'Cross-Origin-Resource-Policy',
+                        value: 'same-origin',
+                    },
                 ],
             },
             {

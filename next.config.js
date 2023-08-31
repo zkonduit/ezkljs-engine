48aff14e5d811f925d19feec8ec36ce37993606f
Ethan Cemer <tylercemer@gmail.com>
2023-08-31 18:11:25 -0500

    *use same-origin for both

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
                 headers: [
                     {
                         key: 'Cross-Origin-Embedder-Policy',
-                        value: 'require-corp',
+                        value: 'same-origin',
                     },
                     {
                         key: 'Cross-Origin-Opener-Policy',

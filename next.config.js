d8b10fd7cc8fb5fa99b6c56d75fe82b67e3e8584
Ethan Cemer <tylercemer@gmail.com>
2023-09-01 15:01:24 -0500

    *rmv first obj in headers array.

--- next.config.js
+++ next.config.js
@@ -3,23 +3,6 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Cross-Origin-Embedder-Policy',
-                        value: 'require-corp',
-                    },
-                    {
-                        key: 'Cross-Origin-Opener-Policy',
-                        value: 'same-origin',
-                    },
-                    {
-                        key: 'Cross-Origin-Resource-Policy',
-                        value: 'cross-origin',
-                    },
-                ],
-            },
-            {
                 // matching all API routes
                 source: "/api/:path*",
                 headers: [

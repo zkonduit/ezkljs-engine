02bf61f4e26e2caf849258193aef7fab0a98c752
Casey <leptone55@gmail.com>
2023-09-01 13:40:13 -0700

    generalize path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
       return [
         {
-          source: '/',
+          source: '/:path*',
           headers: [
             {
               key: 'Cross-Origin-Embedder-Policy',

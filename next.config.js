f3478fd5cecb3704f3404d070bd8ab5e19439298
Casey <leptone55@gmail.com>
2023-09-01 13:47:10 -0700

    remove

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
       return [
         {
-          source: '/:path*',
+          source: '/',
           headers: [
             {
               key: 'Cross-Origin-Embedder-Policy',

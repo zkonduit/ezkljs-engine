76535697415e91ae395bc10bdd8aae4c3481074a
Ethan Cemer <tylercemer@gmail.com>
2023-09-01 16:04:35 -0500

    *original path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
       return [
         {
-          source: '/',
+          source: '/(.*)',
           headers: [
             {
               key: 'Cross-Origin-Embedder-Policy',

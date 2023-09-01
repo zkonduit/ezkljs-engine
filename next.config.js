d31b54a02d28db85b30eac89c35b4bb7d506e93a
Ethan Cemer <tylercemer@gmail.com>
2023-08-31 21:32:49 -0500

    *be more specific with source path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/(.*)',
+                source: '_next/static/chunks/455.c91905ae5809d28a.js',
                 headers: [
                     {
                         key: 'Cross-Origin-Embedder-Policy',

7386c7af22b4f90f2fbd354f64acff07f2b3448e
Ethan Cemer <tylercemer@gmail.com>
2023-09-01 15:39:58 -0500

    *try credentialless

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
           headers: [
             {
               key: 'Cross-Origin-Embedder-Policy',
-              value: 'require-corp',
+              value: 'credentialless',
             },
             {
               key: 'Cross-Origin-Opener-Policy',
